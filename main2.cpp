bca11c087ce876b663225cd79f5a6232d64552d8
eidznyte <144419332+eidznyte@users.noreply.github.com>
2023-10-12 18:26:48 +0300

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -1,48 +1,138 @@
+#include "students2.h"
 #include <iostream>
-#include <vector>
-#include "students2.h" // Including the header for the Student structure and associated functions
+#include <chrono>
+
 
 int main() {
     std::vector<Student> students;
-    while (true) {
-        std::cout << "Choose an option:\n";
-        std::cout << "1. Read data from file\n";
-        std::cout << "2. Input data manually\n";
-        std::cout << "3. Generate random student list files\n";
-        std::cout << "4. Exit\n";
-        std::cout << "Your choice: ";
 
-        int choice;
-        std::cin >> choice;
-        std::cin.ignore();
+    std::cout << "Would you like to: \n"
+        "1. Input data manually \n"
+        "2. Read data from a file \n"
+        "3. Generate a random student list file\n"
+        "Choose an option (1/2/3): ";
 
-        switch (choice) {
-        case 1:
-            readFromFile(students);
-            break;
-        case 2:
-            inputStudentsManually(students);
-            break;
-        case 3:
-            std::cout << "Generating files...\n";
-            generateFile("1000.txt", 1000);
-            generateFile("10000.txt", 10000);
-            generateFile("100000.txt", 100000);
-            generateFile("1000000.txt", 1000000);
-            generateFile("10000000.txt", 10000000);
-            std::cout << "Files generated successfully!\n";
-            break;
-        case 4:
-            return 0;
-        default:
-            std::cout << "Invalid choice. Please try again.\n";
+    int choice;
+    std::cin >> choice;
+
+   
+    std::chrono::high_resolution_clock::time_point startReading, endReading, startSorting, endSorting, startWriting, endWriting;
+
+    switch (choice) {
+    case 1:
+        inputStudentsManually(students);
+        break;
+    case 2:
+    {
+        std::cout << "Enter the filename to read from (or type 'default' for generated files): ";
+        std::string filename;
+        std::cin >> filename;
+
+        if (filename == "default") {
+            std::cout << "How many records in the file? \n"
+                << "1. 1,000 \n"
+                << "2. 10,000 \n"
+                << "3. 100,000 \n"
+                << "4. 1,000,000 \n"
+                << "5. 10,000,000 \n"
+                << "Choose an option (1/2/3/4/5): ";
+            int subChoice;
+            std::cin >> subChoice;
+
+            switch (subChoice) {
+            case 1: filename = "random_students_1000.txt"; break;
+            case 2: filename = "random_students_10000.txt"; break;
+            case 3: filename = "random_students_100000.txt"; break;
+            case 4: filename = "random_students_1000000.txt"; break;
+            case 5: filename = "random_students_10000000.txt"; break;
+            default:
+                std::cerr << "Invalid option!" << std::endl;
+                return 1;
+            }
         }
 
-        // After processing, display results
-        displayStudents(students);
-        students.clear();  // Clearing the vector for next operations
+        startReading = std::chrono::high_resolution_clock::now();
+        readFromFile(students, filename);
+        endReading = std::chrono::high_resolution_clock::now();
+        break;
     }
+    case 3:
+        {
+            std::cout << "How many records would you like to generate? \n"
+                "1. 1,000 \n"
+                "2. 10,000 \n"
+                "3. 100,000 \n"
+                "4. 1,000,000 \n"
+                "5. 10,000,000 \n"
+                "Choose an option (1/2/3/4/5): ";
+            int subChoice;
+            std::cin >> subChoice;
+            size_t records;
+            switch (subChoice) {
+            case 1:
+                records = 1'000;
+                break;
+            case 2:
+                records = 10'000;
+                break;
+            case 3:
+                records = 100'000;
+                break;
+            case 4:
+                records = 1'000'000;
+                break;
+            case 5:
+                records = 10'000'000;
+                break;
+            default:
+                std::cerr << "Invalid option!" << std::endl;
+                return 1;
+            }
+            std::string filename = "random_students_" + std::to_string(records) + ".txt";
+            generateFile(filename, records);
+            break;
+        }
+    default:
+        std::cerr << "Invalid choice!" << std::endl;
+        return 1;
+    }
+
+    if (choice != 3) {
+        displayStudents(students);
+    }
+
+    std::vector<Student> dummies;
+    std::vector<Student> smart;
+
+    startSorting = std::chrono::high_resolution_clock::now();
+    categorizeStudents(students, dummies, smart);
+    endSorting = std::chrono::high_resolution_clock::now();
+
+    
+    std::cout << "\nDummies:\n";
+    displayStudents(dummies);
+
+    std::cout << "\nSmart:\n";
+    displayStudents(smart);
+
+    
+    startWriting = std::chrono::high_resolution_clock::now();
+    writeToFile(dummies, "dummies.txt");
+    writeToFile(smart, "smart.txt");
+    endWriting = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Students have been categorized and written to 'dummies.txt' and 'smart.txt' respectively." << std::endl;
+
+    
+    std::chrono::duration<double> readingDuration = endReading - startReading;
+    std::chrono::duration<double> sortingDuration = endSorting - startSorting;
+    std::chrono::duration<double> writingDuration = endWriting - startWriting;
+
+    std::cout << "\nTime taken for reading data: " << readingDuration.count() << " seconds." << std::endl;
+    std::cout << "Time taken for sorting students: " << sortingDuration.count() << " seconds." << std::endl;
+    std::cout << "Time taken for writing to files: " << writingDuration.count() << " seconds." << std::endl;
 
     return 0;
 }
+
 
