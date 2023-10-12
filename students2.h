c985f48e0606849ea7c7cb903d0439fa6c94fdc2
eidznyte <144419332+eidznyte@users.noreply.github.com>
2023-10-12 18:27:14 +0300

    Update students2.h

--- students2.h
+++ students2.h
@@ -1,25 +1,28 @@
 #ifndef STUDENTS2_H
 #define STUDENTS2_H
-
-#include <vector>
+#pragma once
 #include <string>
+#include <vector>
 
 struct Student {
     std::string name;
     std::string surname;
     std::vector<int> homeworks;
-    int exam;
-    double finalScoreAvg;
-    double finalScoreMed;
+    int exam = 0;
+    double finalScoreAvg = 0;
+    double finalScoreMed = 0;
 };
 
-void readFromFile(std::vector<Student>& students);
 void inputStudentsManually(std::vector<Student>& students);
 void displayStudents(const std::vector<Student>& students);
 void generateFile(const std::string& filename, int numStudents);
+void generateRandomScores(Student& student);
+void categorizeStudents(const std::vector<Student>& students, std::vector<Student>& dummies, std::vector<Student>& smart);
+void writeToFile(const std::vector<Student>& students, const std::string& filename);
+void readFromFile(std::vector<Student>& students, const std::string& filename);
 double calculateAverage(const std::vector<int>& grades);
 double calculateMedian(std::vector<int> grades);
-void generateRandomScores(Student& student);
+
+
 
 #endif // STUDENTS2_H
-
